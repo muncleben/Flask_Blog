62fb259792a9fed5bf0c2068c98e8a61fe6f6dd5
Uncle Ben <jiasz527@qq.com>
2020-04-01 22:28:34 +0800

    Add db model.

--- flaskblog.py
+++ flaskblog.py
@@ -1,24 +1,52 @@
 from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
 
 app = Flask(__name__)
-
 app.config['SECRET_KEY']='aab17fddf3a164d1073b5042acfbace4'
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///site.db'
+db = SQLAlchemy(app)
 
-posts = [
-    {
-        'author': 'UncleBen',
-        'title': 'Blog post 1',
-        'content': 'First blog content.',
-        'date_posted': 'March 28, 2020'
-    },
-    {
-        'author': 'Erick',
-        'title': 'Blog post 2',
-        'content': 'Second blog content.',
-        'date_posted': 'March 26, 2020'
-    }
-]
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    posts = db.relationship('Post', backref='author', lazy=True)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+        
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.date_posted}')"
+
+
+# posts = [
+#     {
+#         'author': 'UncleBen',
+#         'title': 'Blog post 1',
+#         'content': 'First blog content.',
+#         'date_posted': 'March 28, 2020'
+#     },
+#     {
+#         'author': 'Erick',
+#         'title': 'Blog post 2',
+#         'content': 'Second blog content.',
+#         'date_posted': 'March 26, 2020'
+#     }
+# ]
 
 
 @app.route('/')
